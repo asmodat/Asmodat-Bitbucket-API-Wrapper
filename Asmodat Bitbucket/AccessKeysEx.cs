0b947d38c6b6968aa8fd93d8565304a8d87097f7
asmodat <asmodat@gmail.com>
2021-01-05 22:05:55 +0000

    NUGET Package Update

---
+++ Asmodat Bitbucket/AccessKeysEx.cs
@@ -0,0 +1,29 @@
+﻿using AsmodatStandard.Extensions;
+using AsmodatStandard.Extensions.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsmodatBitbucket
+{
+    public static class AccessKeysEx
+    {
+        /*public static async Task PullRequestComment(this BitbucketClient client, PullRequest pullRequest, string content)
+        {
+            var list = new List<PullRequest>();
+            var token = await client.GetAccessAsync();
+            var url = $"{client.ApiUrl}/2.0/repositories/{username}/{repo_slug}/pullrequests";
+
+
+            var requestUri = pullRequest.links.comments.href.Replace("api.bitbucket.org/2.0/", "bitbucket.org/!api/1.0/");
+            var response = await HttpHelper.POST(
+            requestUri: requestUri,
+            content: new StringContent($"{{\"content\": \"{content}\"}}", Encoding.UTF8, "application/json"),
+            ensureStatusCode: System.Net.HttpStatusCode.OK,
+            defaultHeaders: new(string, string)[] { ("Authorization", $"Bearer {token.access_token}") });
+        }*/
+    }
+}
---
+++ Asmodat Bitbucket/ProjetcsEx.cs
@@ -0,0 +1,39 @@
+﻿using AsmodatStandard.Extensions;
+using AsmodatStandard.Extensions.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsmodatBitbucket
+{
+    public static class ProjectsEx
+    {
+
+        /*public static async Task<bool> Create(this BitbucketClient client)
+        {
+            var list = new List<PullRequest>();
+            var token = await client.GetAccessAsync();
+
+            var response = await HttpHelper.POST<PullRequestApprove>(
+            requestUri: "https://bitbucket.org/account/projects/create?owner=SettleApps",
+            content: new StringContent($"{{\"content\": \"{content}\"}}", Encoding.UTF8, "application/json"),
+            ensureStatusCode: null,
+            defaultHeaders: new(string, string)[] { ("Authorization", $"Bearer {token.access_token}") });
+
+           if (response?.error?.message.Contains("already approved") == true)
+                if (response?.error != null)
+                {
+                    if (response?.error?.message != null && response.error.message.Contains("already approved"))
+                        return true;
+
+                    throw new Exception($"Failed Get request: '{pullRequest.links.approve.href}', respose: {response.JsonSerialize()}");
+                }
+                
+            return response.approved;
+        }*/
+
+    }
+}
